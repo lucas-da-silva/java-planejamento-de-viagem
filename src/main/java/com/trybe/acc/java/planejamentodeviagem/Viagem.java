266ddbcbf8f1cc4887eb3c7d3d1ad0cf7a30b007
Lucas da Silva <lucassouza00012345@gmail.com>
2023-08-10 16:40:04 -0300

    feat: implementa os métodos Construtor, retornarDuracaoVoo e retornarInformacaoViagem

--- src/main/java/com/trybe/acc/java/planejamentodeviagem/Viagem.java
+++ src/main/java/com/trybe/acc/java/planejamentodeviagem/Viagem.java
@@ -1,6 +1,7 @@
 package com.trybe.acc.java.planejamentodeviagem;
 
 public class Viagem {
+
   private String embarque;
   private String origem;
   private String destino;
@@ -9,15 +10,17 @@ public class Viagem {
 
   /**
    * Método construtor da classe.
-   * 
    */
-  public Viagem() {
-    /* Implemente sua solução aqui (você pode ter que alterar o construtor) */
+  public Viagem(String embarque, String origem, String destino, double distanciaKm) {
+    this.embarque = embarque;
+    this.origem = origem;
+    this.destino = destino;
+    this.distanciaKm = distanciaKm;
+    this.voo = new Voo();
   }
 
   /**
    * Método para retorno do horário de Desembarque no Destino.
-   * 
    */
   public String retonarDesembarqueHorarioLocalDestino() {
     Tempo tempoViagem =
@@ -27,17 +30,23 @@ public class Viagem {
 
   // aqui consideramos que um aviao percorre em media 700 km a cada hora
   public int retornarDuracaoVoo() {
-      /* Implemente sua solução aqui */
+    return this.voo.retornarTempoVoo(this.distanciaKm);
   }
 
   /**
    * Método para retorno da informaçao da viagem.
-   * 
    */
   public String retornarInformacaoViagem() {
     Tempo tempoViagem =
         new Tempo(this.embarque, this.origem, this.destino, this.retornarDuracaoVoo());
+    String desembarqueHorarioDestino = tempoViagem.retonarDesembarqueHorarioLocalDestino();
+    String informacaoDeVoo = this.voo.retornarInformacaoVoo(this.embarque, this.origem,
+        desembarqueHorarioDestino, this.destino);
 
-    /* Implemente sua solução aqui */
+    return informacaoDeVoo
+        + "Atenção: o desembarque em Tokyo será: " + desembarqueHorarioDestino
+        + " no horário de " + this.destino + " e "
+        + tempoViagem.retonarDesembarqueHorarioLocalOrigem()
+        + " no horário de " + this.origem;
   }
 }
