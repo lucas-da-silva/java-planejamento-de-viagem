b29cf65e20f02a8b5382671e0cbaa70ce3fd99f2
Lucas da Silva <lucassouza00012345@gmail.com>
2023-08-10 16:40:20 -0300

    feat: implementa os métodos retornarTempoVoo e retornarInformacaoVoo

--- src/main/java/com/trybe/acc/java/planejamentodeviagem/Voo.java
+++ src/main/java/com/trybe/acc/java/planejamentodeviagem/Voo.java
@@ -1,25 +1,26 @@
 package com.trybe.acc.java.planejamentodeviagem;
 
 public class Voo {
+
   private int tempoVoo;
 
   /**
    * Método para retornar tempo de Voo.
-   * 
    */
   public int retornarTempoVoo(double distanciaKm) {
-
-    Double duracaoDouble = distanciaKm / 700.0;
-
-    /* Implemente sua solução aqui */
+    double duracaoDouble = distanciaKm / 700.0;
+    return (int) duracaoDouble;
   }
 
   /**
    * Método para retornar informaçao do Voo.
-   * 
    */
   public String retornarInformacaoVoo(String embarque, String origem, String desembarque,
       String destino) {
-    /* Implemente sua solução aqui */
+
+    return "Partida: " + embarque + "\n"
+        + "Origem: " + origem + "\n \n"
+        + "Chegada: " + desembarque + "\n"
+        + "Destino: " + destino + "\n \n";
   }
 }
